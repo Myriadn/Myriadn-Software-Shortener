88e52d6991a7ac195b7fb158c9b4abf62c79c32e
nuke cola <manaska129@gmail.com>
2024-10-07 23:07:52 +0700

    Initial commit

---
+++ src/services/users.ts
@@ -0,0 +1,51 @@
+import httpError from "http-errors";
+import { validateLogin, validateRegister } from "./validations";
+import knex from "../config/knex";
+import { comparePassword, hashPassword } from "../config/encryption";
+import { generateToken } from "../config/jwt";
+
+const getUser = async (username: string) =>
+  knex("users").whereRaw(`LOWER(username) = LOWER(?)`, [username]).first();
+
+export const register = async (body: {
+  username: string;
+  password: string;
+}) => {
+  validateRegister(body);
+  const current_user = await getUser(body.username);
+  if (current_user) {
+    throw new httpError.Conflict("Username sudah terpakai");
+  }
+  const user = (
+    await knex("users").insert(
+      {
+        username: body.username.toLocaleLowerCase(),
+        password: await hashPassword(body.password),
+      },
+      ["id", "username"]
+    )
+  )[0];
+  return user;
+};
+
+export const login = async (body: { username: string; password: string }) => {
+  validateLogin(body);
+  const user = await getUser(body.username);
+  if (!user) {
+    throw new httpError.Unauthorized("Username atau Password salah");
+  }
+  const passwordMatch = await comparePassword(body.password, user.password);
+  if (!passwordMatch) {
+    throw new httpError.Unauthorized("Username atau Password salah");
+  }
+  const token = await generateToken({ id: user.id });
+  return {
+    user: {
+      id: user.id,
+      username: user.username,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    },
+    token,
+  };
+};
---
+++ src/services/validations.ts
@@ -0,0 +1,41 @@
+import Validator from "validatorjs";
+import httpError from "http-errors";
+
+type RequestBody = { [key: string]: any };
+
+const validateBody = (body: RequestBody, validationSchema: Validator.Rules) => {
+  let validation = new Validator(body, validationSchema);
+  if (validation.fails()) {
+    const errors = validation.errors.all();
+    const aggregatedErrors: string[] = [];
+    Object.keys(errors).forEach((key) => {
+      aggregatedErrors.push(validation.errors.first(key) as string);
+    });
+    throw new httpError.BadRequest(aggregatedErrors.join(" , "));
+  } else {
+    return true;
+  }
+};
+
+export const validateCreateShortURL = async (body: RequestBody) =>
+  validateBody(body, {
+    url: "url|required",
+    id: "string|min:5|max:10|not_in:urls,visits,auth",
+  });
+
+export const validateUpdateShortURL = async (body: RequestBody) =>
+  validateBody(body, {
+    url: "url|required",
+  });
+
+export const validateRegister = (body: RequestBody) =>
+  validateBody(body, {
+    username: "string|required|min:4|max:8",
+    password: "string|required|min:6",
+  });
+
+export const validateLogin = (body: RequestBody) =>
+  validateBody(body, {
+    username: "string|required",
+    password: "string|required",
+  });
